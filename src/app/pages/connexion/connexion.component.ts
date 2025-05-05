a9550d41fae9aa081a8e059c5cd72727390c0582
fbansept <bansept.franck@gmail.com>
2025-05-05 14:24:55 +0200

    connexion

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-connexion',
-  imports: [MatButtonModule, MatInputModule, FormsModule],
+  imports: [MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss',
 })
 export class ConnexionComponent {
+  formBuilder = inject(FormBuilder);
+  http = inject(HttpClient);
+
+  formulaire = this.formBuilder.group({
+    email: ['a@a', [Validators.email, Validators.required]],
+    password: ['root', [Validators.required]],
+  });
+
   onConnexion() {
-    console.log('submit');
+    if (this.formulaire.valid) {
+      this.http
+        .post('http://localhost:5000/connexion', this.formulaire.value)
+        .subscribe((resultat) => console.log(resultat));
+    }
   }
 }
